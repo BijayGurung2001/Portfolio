afc4cdc83d01d904330acf734e1960702a475dab
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-04 20:49:00 +0545

    change in basename

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter basename='Portfolio'>
+     <div className="App">
       <Home/>
     </div>
+    </BrowserRouter>
+   
   );
 }
 

d40a93de73c21ab6f1a094a1f9ac8b4bf0cf3f6f
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-10 21:29:04 +0545

    change in main function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Pages/Home';
 function App() {
   return (
     <HashRouter >
-      <router basename={process.env.PUBLIC_URL}>
+      <router basename='/Profile'>
           <Routes>
           <Route path="/" element={<Home />} />
                   </Routes>
